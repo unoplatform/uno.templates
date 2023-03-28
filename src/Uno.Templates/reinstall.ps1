b64bbc371ff4d00cdf007e23d061cb4c04da246f
Dan Siegel <me@dansiegel.net>
2023-03-28 06:30:42 -0600

    chore: rename csproj & bundle Uno templates

--- src/Uno.Templates/reinstall.ps1
+++ src/Uno.Templates/reinstall.ps1
@@ -1,15 +1,35 @@
 ﻿param(
     # Version of published Uno.Extensions packages
-    [string]$Version = "255.255.255.255"
+    [string]$ExtensionsVersion = "2.3.2",
+
+    # Version of published Uno.WinUI packages
+    [string]$UnoVersion = "4.8.15"
 )
-dotnet new uninstall Uno.Extensions.Templates
+
+function RemoveNuGetPackage {
+    param (
+        [string]$PackageName
+    )
+    
+    $installed = (dotnet new uninstall) -join ","
+    if ($installed -like "*$PackageName*") {
+        dotnet new uninstall $PackageName
+    }
+}
+
+RemoveNuGetPackage -PackageName "Uno.Templates"
+
+# Ensure legacy pacakges are removed
+RemoveNuGetPackage -PackageName "Uno.Extensions.Templates"
+RemoveNuGetPackage -PackageName "Uno.ProjectTemplates.Dotnet"
 
 # Remove artifacts from previous builds
 Get-ChildItem .\ -Include bin,obj -Recurse | ForEach-Object ($_) { Remove-Item $_.Fullname -Force -Recurse }
 
-dotnet build -p:Version=$Version -c Release
+dotnet build -p:Version=$UnoVersion -p:UnoVersion=$UnoVersion -p:UnoExtensionsVersion=$ExtensionsVersion -c Release
 if($LASTEXITCODE -ne 0) {
     Write-Error "Building NuGet Package failed."
     exit $LASTEXITCODE
 }
-dotnet new install $PSScriptRoot\bin\Uno.Extensions.Templates\Release\Uno.Extensions.Templates.$Version.nupkg
+dotnet new install $PSScriptRoot\bin\Release\Uno.Templates.$UnoVersion.nupkg
+
