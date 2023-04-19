69a75697785aff8c5eacf08410190fb6c2faf172
Nick Randolph <nick@builttoroam.com>
2023-04-19 14:59:13 +1000

    fix: Correct markup

--- src/Uno.Templates/content/unoapp/MyExtensionsApp.1/Presentation/Shell.xaml.cs
+++ src/Uno.Templates/content/unoapp/MyExtensionsApp.1/Presentation/Shell.xaml.cs
@@ -8,34 +8,36 @@ public sealed partial class Shell : UserControl, IContentControlProvider
 //+:cnd:noEmit
 #if useCsharpMarkup
 		this.Content(
-			new Border(
-#if useToolkit
-				new ExtendedSplashScreen()
-					.Assign(out var splash)
-					.HorizontalAlignment(HorizontalAlignment.Stretch)
-					.VerticalAlignment(VerticalAlignment.Stretch)
-					.HorizontalContentAlignment(HorizontalAlignment.Stretch)
-					.VerticalContentAlignment(VerticalAlignment.Stretch)
-					.LoadingContentTemplate<object>(_ => new Grid()
-						.RowDefinitions(new GridLength(2, GridUnitType.Star), new GridLength(1, GridUnitType.Star))
-						.Children(
-							new ProgressRing()
-								.Grid(row: 1)
-								.VerticalAlignment(VerticalAlignment.Center)
-								.HorizontalAlignment(HorizontalAlignment.Center)
-								.Height(100)
-								.Width(100)
+			new Border()
+				.Child(
+	#if useToolkit
+					new ExtendedSplashScreen()
+						.Assign(out var splash)
+						.HorizontalAlignment(HorizontalAlignment.Stretch)
+						.VerticalAlignment(VerticalAlignment.Stretch)
+						.HorizontalContentAlignment(HorizontalAlignment.Stretch)
+						.VerticalContentAlignment(VerticalAlignment.Stretch)
+						.LoadingContentTemplate<object>(_ => new Grid()
+							.RowDefinitions(new GridLength(2, GridUnitType.Star), new GridLength(1, GridUnitType.Star))
+							.Children(
+								new ProgressRing()
+									.Grid(row: 1)
+									.VerticalAlignment(VerticalAlignment.Center)
+									.HorizontalAlignment(HorizontalAlignment.Center)
+									.Height(100)
+									.Width(100)
+							)
 						)
-					)
-#else
-				new ContentControl()
-					.Assign(out var splash)
-					.HorizontalAlignment(HorizontalAlignment.Stretch)
-					.VerticalAlignment(VerticalAlignment.Stretch)
-					.HorizontalContentAlignment(HorizontalAlignment.Stretch)
-					.VerticalContentAlignment(VerticalAlignment.Stretch)
-#endif
-				).Background(Theme.Brushes.Background.Default)
+	#else
+					new ContentControl()
+						.Assign(out var splash)
+						.HorizontalAlignment(HorizontalAlignment.Stretch)
+						.VerticalAlignment(VerticalAlignment.Stretch)
+						.HorizontalContentAlignment(HorizontalAlignment.Stretch)
+						.VerticalContentAlignment(VerticalAlignment.Stretch)
+	#endif
+				)
+				.Background(Theme.Brushes.Background.Default)
 			);
 		ContentControl = splash;
 #else
