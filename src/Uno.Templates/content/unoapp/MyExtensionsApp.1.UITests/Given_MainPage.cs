c6cbdfd0a8f30b66d0f4995169802d68dfa9de3e
Nick Randolph <nick@builttoroam.com>
2023-04-28 16:27:32 +1000

    chore: Fix build error with async method

--- src/Uno.Templates/content/unoapp/MyExtensionsApp.1.UITests/Given_MainPage.cs
+++ src/Uno.Templates/content/unoapp/MyExtensionsApp.1.UITests/Given_MainPage.cs
@@ -3,17 +3,18 @@
 public class Given_MainPage : TestBase
 {
 	[Test]
-	public void When_SmokeTest()
+	public async Task When_SmokeTest()
 	{
 		// NOTICE
 		// To run UITests, Run the WASM target without debugger. Note
 		// the port that is being used and update the Constants.cs file
 		// in the UITests project with the correct port number.
 
-//+:cnd:noEmit
-#if (useExtensionsNavigation)
 		// Add delay to allow for the splash screen to disappear
 		await Task.Delay(5000);
+
+//+:cnd:noEmit
+#if (useExtensionsNavigation)
 
 		// Query for the SecondPageButton and then tap it
 		Query xamlButton = q => q.All().Marked("SecondPageButton");
