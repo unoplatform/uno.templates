d73b41cc16ae011cfafdc7c7c46fa4c75b2bf74a
Nick Randolph <nick@builttoroam.com>
2023-05-02 00:45:14 +1000

    chore: Fix MSAL refs

---
+++ src/Uno.Templates/content/unoapp/MyExtensionsApp._1.Mobile/Android/MsalActivity.Android.cs
@@ -0,0 +1,18 @@
+//-:cnd:noEmit
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+using Microsoft.Identity.Client;
+
+namespace MyExtensionsApp._1.Droid;
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(
+	new[] { Android.Content.Intent.ActionView },
+	Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+	DataScheme = "myprotocol")]
+public class MsalActivity : BrowserTabActivity
+{
+}
