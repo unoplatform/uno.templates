601a007ef8ce98dbfe0913de1d8fa86787961517
Dan Siegel <me@dansiegel.net>
2023-05-11 20:46:46 -0600

    chore: rename Window as public property

--- src/Uno.Templates/content/unoapp/MyExtensionsApp._1/App.blank.cs
+++ src/Uno.Templates/content/unoapp/MyExtensionsApp._1/App.blank.cs
@@ -3,25 +3,25 @@ namespace MyExtensionsApp._1;
 
 public class App : Application
 {
-	public static Window? _window;
+	public static Window? AppWindow { get; private set; }
 
 	protected override void OnLaunched(LaunchActivatedEventArgs args)
 	{
 #if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
-		_window = new Window();
+		AppWindow = new Window();
 #else
-		_window = Microsoft.UI.Xaml.Window.Current;
+		AppWindow = Microsoft.UI.Xaml.Window.Current;
 #endif
 
 		// Do not repeat app initialization when the Window already has content,
 		// just ensure that the window is active
-		if (_window.Content is not Frame rootFrame)
+		if (AppWindow.Content is not Frame rootFrame)
 		{
 			// Create a Frame to act as the navigation context and navigate to the first page
 			rootFrame = new Frame();
 
 			// Place the frame in the current Window
-			_window.Content = rootFrame;
+			AppWindow.Content = rootFrame;
 
 			rootFrame.NavigationFailed += OnNavigationFailed;
 		}
@@ -35,7 +35,7 @@ public class App : Application
 		}
 
 		// Ensure the current window is active
-		_window.Activate();
+		AppWindow.Activate();
 	}
 
 	/// <summary>
