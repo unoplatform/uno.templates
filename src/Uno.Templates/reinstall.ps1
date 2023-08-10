ecb113ca75c5fd07f191e6c19dff75087baf6437
Dan Siegel <me@dansiegel.net>
2023-08-10 07:50:53 -0600

    feat: support injecting Maui Styles
    (cherry picked from commit bace60bce784c0f039e78553d4b2e1a0921734b7)
    
    # Conflicts:
    #	src/Uno.Templates/Uno.Templates.csproj
    #	src/Uno.Templates/reinstall.ps1

--- src/Uno.Templates/reinstall.ps1
+++ src/Uno.Templates/reinstall.ps1
@@ -3,7 +3,11 @@ param(
     [string]$TemplatesVersion = "255.255.255.255",
 
     # Version of published Uno.Extensions packages
+<<<<<<< HEAD
     [string]$ExtensionsVersion = "2.5.0-dev.148",
+=======
+    [string]$ExtensionsVersion = "3.0.0-dev.2142",
+>>>>>>> bace60b (feat: support injecting Maui Styles)
 
     # Version of published Uno.WinUI packages
     [string]$UnoVersion = "4.9.20"
