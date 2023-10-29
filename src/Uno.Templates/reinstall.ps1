d15eb075813510fd81c867d03e2bc519d262f74b
Jerome Laban <jerome.laban@nventive.com>
2023-10-29 16:37:52 -0400

    chore: Bump to latest stable

--- src/Uno.Templates/reinstall.ps1
+++ src/Uno.Templates/reinstall.ps1
@@ -3,7 +3,7 @@ param(
     [string]$TemplatesVersion = "255.255.255.255",
 
     # Version of published Uno.Extensions packages
-    [string]$ExtensionsVersion = "3.0.0-dev.2335",
+    [string]$ExtensionsVersion = "3.0.8",
 
     # Version of published Uno.WinUI packages
     [string]$UnoVersion = "5.0.13"
