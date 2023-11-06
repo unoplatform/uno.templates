6e7e3e13f695702f4ef214f5e8496810ddd67c82
Jerome Laban <jerome.laban@nventive.com>
2023-11-06 15:53:53 -0500

    chore: update to latest stable packages

--- src/Uno.Templates/reinstall.ps1
+++ src/Uno.Templates/reinstall.ps1
@@ -3,10 +3,10 @@ param(
     [string]$TemplatesVersion = "255.255.255.255",
 
     # Version of published Uno.Extensions packages
-    [string]$ExtensionsVersion = "3.0.10",
+    [string]$ExtensionsVersion = "3.0.11",
 
     # Version of published Uno.WinUI packages
-    [string]$UnoVersion = "5.0.19"
+    [string]$UnoVersion = "5.0.31"
 )
 
 function RemoveNuGetPackage {
