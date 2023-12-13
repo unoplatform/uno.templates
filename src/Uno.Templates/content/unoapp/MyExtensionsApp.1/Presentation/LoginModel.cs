3eedd0314259768398de6b1a393fe50b3d6261eb
LuisAlfredo92 <92luisalfredo@protonmail.com>
2023-12-12 19:41:07 -0600

    Fixing custon login logic template
    -Fixing Password binding
    -Adding "Mode=TwoWay" to fix binding
    -Fixing identation

--- src/Uno.Templates/content/unoapp/MyExtensionsApp.1/Presentation/LoginModel.cs
+++ src/Uno.Templates/content/unoapp/MyExtensionsApp.1/Presentation/LoginModel.cs
@@ -7,7 +7,7 @@ public partial record LoginModel(IDispatcher Dispatcher, INavigator Navigator, I
 
  //+:cnd:noEmit
 #if useCustomAuthentication
-   public IState<string> Username => State<string>.Value(this, () => string.Empty);
+    public IState<string> Username => State<string>.Value(this, () => string.Empty);
 
     public IState<string> Password => State<string>.Value(this, () => string.Empty);
 #endif
