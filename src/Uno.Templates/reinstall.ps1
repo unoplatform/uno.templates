5a8d0588f407f2619fea069ab1ad6d04c32e91ac
Dan Siegel <me@dansiegel.net>
2024-01-22 07:27:47 -0600

    chore: update for default includes

--- src/Uno.Templates/reinstall.ps1
+++ src/Uno.Templates/reinstall.ps1
@@ -6,7 +6,7 @@ param(
     [string]$ExtensionsVersion = "4.0.0-dev.212",
 
     # Version of published Uno.WinUI packages
-    [string]$UnoVersion = "5.1.0-dev.1459"
+    [string]$UnoVersion = "5.2.0-dev.24"
 )
 
 function RemoveNuGetPackage {
