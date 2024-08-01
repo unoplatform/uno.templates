d574e57bcb321e4f3083f34ac5a5be18622a6f6b
Jerome Laban <jerome@platform.uno>
2024-08-01 08:28:47 -0400

    fix: Upgrade core packages

--- tools/Uno.Sdk.Updater/Program.cs
+++ tools/Uno.Sdk.Updater/Program.cs
@@ -286,7 +286,7 @@ static string GetManifestGroupVersionOverride(IEnumerable<ManifestGroup> manifes
 
 static async Task<ManifestGroup> UpdateGroup(ManifestGroup group, NuGetVersion unoVersion, NuGetApiClient client)
 {
-    if (group.Packages.Any(x => x.StartsWith("Xamarin")) || group.Group == "Core")
+    if (group.Packages.Any(x => x.StartsWith("Xamarin")))
     {
         Console.WriteLine("Leaving group as is: " + group.Group);
         return group;
